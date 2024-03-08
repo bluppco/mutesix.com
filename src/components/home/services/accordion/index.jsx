d3df90ba8c4ae044b6c7f306c1e91b184b949ce6
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-03-08 19:21:13 +0530

    feat: optimized home page for mobile

--- src/components/home/services/accordion/index.jsx
+++ src/components/home/services/accordion/index.jsx
@@ -22,7 +22,7 @@ const Accordion = ( props ) => {
     const isOpen = index === expand
 
     return (
-        <div className="py-8 px-12 bg-white z-30 my-8 rounded-[42px]">
+        <div className="py-6 md:py-8 px-8 md:px-12 bg-white z-30 my-8 rounded-[42px]">
             <motion.section
                 initial={ false }
                 onClick={() => updateExpand( isOpen ? false : index )}
@@ -30,7 +30,7 @@ const Accordion = ( props ) => {
                 un9n-event={ "service-" + index }
             >
                 <div className="flex-wrap">
-                    <p className="text-2xl md:text-3xl font-black text-[#071641]">{ value.title }</p>
+                    <p className="text-3xl font-black text-[#071641]">{ value.title }</p>
                 </div>
                 <div className="w-6 aspect-square">
                     <motion.img
@@ -55,8 +55,8 @@ const Accordion = ( props ) => {
                         }}
                         transition={{ duration: 0.2 }}
                     >
-                        <div className="md:w-[50%] pt-6 space-y-6">
-                            <p className="font-proxima_nova text-lg md:text-2xl text-ms_dark_blue md:leading-normal">{ value.description }</p>
+                        <div className="md:w-[50%] pt-4 md:pt-6 space-y-4 md:space-y-6">
+                            <p className="font-proxima_nova text-xl md:text-2xl text-ms_dark_blue md:leading-normal">{ value.description }</p>
                             <Link href={ value.button_link } aria_label={ value.button_text }>
                                 <PrimaryButton>{ value.button_text }</PrimaryButton>
                             </Link>
